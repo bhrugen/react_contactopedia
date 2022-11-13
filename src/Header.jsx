514cdc98323edbb9bea9a92df68ddc135fd61460
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-11-13 15:30:58 -0600

    Create Project - ContactOPedia

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import logo from "./images/react.png";
+
+function Header() {
+  return (
+    <div className="py-2 pl-2" style={{ borderBottom: "1px solid #777" }}>
+      <img src={logo} alt="" style={{ height: "35px", verticalAlign: "top" }} />
+      <span className="h2 pt-4 m-2 text-white-50">ContactOpedia</span>
+    </div>
+  );
+}
+
+export default Header;
