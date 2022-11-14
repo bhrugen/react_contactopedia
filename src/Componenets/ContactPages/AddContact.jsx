d16ceaa14e5c8a7b3ff854feb84573a2c4e15060
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-11-14 15:10:36 -0600

    Add Contact UI

--- src/Componenets/ContactPages/AddContact.jsx
+++ src/Componenets/ContactPages/AddContact.jsx
@@ -1,7 +1,28 @@
 const AddContact = () => {
   return (
-    <div>
-      <button className="btn btn-secondary form-control">Add Contact</button>
+    <div className="border row text-white p-2">
+      <div className="col-12 text-white-50">Add a new Contact</div>
+      <div className="col-12 col-md-4 p-1">
+        <input
+          className="form-control form-control-sm"
+          placeholder="Name..."
+        ></input>
+      </div>
+      <div className="col-12 col-md-4 p-1">
+        <input
+          className="form-control form-control-sm"
+          placeholder="Email..."
+        ></input>
+      </div>
+      <div className="col-12 col-md-4 p-1">
+        <input
+          className="form-control form-control-sm"
+          placeholder="Phone..."
+        ></input>
+      </div>
+      <div className="col-12 col-md-6 offset-md-3 p-1">
+        <button className="btn btn-primary btn-sm form-control">Create</button>
+      </div>
     </div>
   );
 };
