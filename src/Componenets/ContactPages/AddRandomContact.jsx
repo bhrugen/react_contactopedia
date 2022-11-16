b86b073ed576e6e87e77b5e1a23ea4b975536249
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-11-16 15:34:56 -0600

    Axios Call

--- src/Componenets/ContactPages/AddRandomContact.jsx
+++ src/Componenets/ContactPages/AddRandomContact.jsx
@@ -1,7 +1,17 @@
+import { getRandomUser } from "../../Utility/api";
+
+const GetRandomContact = async () => {
+  const responseFromAPI = await getRandomUser();
+  console.log(responseFromAPI);
+};
+
 const AddRandomContact = () => {
   return (
     <div>
-      <button className="btn btn-success form-control">
+      <button
+        className="btn btn-success form-control"
+        onClick={() => GetRandomContact()}
+      >
         Add Random Contact
       </button>
     </div>
--- src/Utility/api.jsx
+++ src/Utility/api.jsx
@@ -1,1 +1,16 @@
 import axios from "axios";
+
+const getRandomUser = async () => {
+  const response = await axios.get(
+    "https://random-data-api.com/api/v2/users?size=2&is_xml=true",
+    {
+      headers: {},
+      params: {
+        size: 1,
+      },
+    }
+  );
+  return response;
+};
+
+export { getRandomUser };
